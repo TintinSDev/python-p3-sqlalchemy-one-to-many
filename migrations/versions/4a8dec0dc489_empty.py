f57dc7156ccdcc0d6cb4b92f70afc217a6997710
Martin <mmaina290@gmail.com>
2024-02-07 18:08:11 +0300

    migrations

---
+++ migrations/versions/4a8dec0dc489_empty.py
@@ -0,0 +1,24 @@
+"""Empty
+
+Revision ID: 4a8dec0dc489
+Revises: 
+Create Date: 2024-02-07 18:07:21.773665
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4a8dec0dc489'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
